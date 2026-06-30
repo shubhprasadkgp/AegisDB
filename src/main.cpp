35e8f2bfb21705e2ed0b0e4888500f151359f6f3
shubhprasadkgp <shubhprasadkgp@gmail.com>
2026-06-30 16:21:50 +0530

    Implement in-memory key-value store with MemTable abstraction

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include "db.h"
 
 int main() {
-    std::cout << "PebbleDB initialized!" << std::endl;
+    PebbleDB db;
+    Entry result;
+
+    // Test put()
+    db.put("name", "Shubh");
+    db.put("college", "IITKGP");
+
+    // Test get()
+    if (db.get("name", result))
+        std::cout << "Name: " << result.value << '\n';
+
+    if (db.get("college", result))
+        std::cout << "College: " << result.value << '\n';
+
+    // Test missing key
+    if (!db.get("city", result))
+        std::cout << "City not found\n";
+
+    // Test remove()
+    db.remove("name");
+
+    if (db.get("name", result))
+        std::cout << "Deleted: " << result.deleted << '\n';
+
+    // Test reinsert
+    db.put("name", "Shubh Prasad");
+
+    if (db.get("name", result))
+        std::cout << result.value << " " << result.deleted << '\n';
+
     return 0;
 }
